e7a961708736fdc74d82ad26b45f78acdbae5bce
CoriMyp <81583396+CoriMyp@users.noreply.github.com>
2023-01-13 00:45:34 +0200

    Update standart.lua

--- Standart/standart.lua
+++ Standart/standart.lua
@@ -1,1 +1,25 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/SolarisUI.lua"))()
+
+local SupportedGames = {
+}
+local code
+
+for k, v in pairs(SupportedGames) do
+	if v == game.placeId then
+		code = loadstring("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/" .. k .. "/ui.lua")
+		break
+	end
+
+	code = loadstring(game:HttpGet("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/Standart/standart.lua"))()
+end
+
+local win = lib:New({
+	Name = "Paradox Hub",
+	FolderToSave = "ParadoxHub"
+})
+
+local PlayerTab = win:Tab("Player")
+
+
+code:run(PlayerTab)
 
