e806f3a7e9488eda2c97bee5441d0256c015375e
CoriMyp <81583396+CoriMyp@users.noreply.github.com>
2023-01-13 00:46:10 +0200

    Update standart.lua

--- Standart/standart.lua
+++ Standart/standart.lua
@@ -1,25 +1,41 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/SolarisUI.lua"))()
+local source = loadstring(game:HttpGet("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/Standart/source.lua"))()
 
-local SupportedGames = {
-}
-local code
+function run(tab)
+	local MoveSec = tab:Section("Movement")
 
-for k, v in pairs(SupportedGames) do
-	if v == game.placeId then
-		code = loadstring("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/" .. k .. "/ui.lua")
-		break
-	end
+	MoveSec:Slider("Walk speed", 16, 300, 0, 1, "WalkSpeed Slider", function(value)
+		source.walkspeed_value = value
+	end)
 
-	code = loadstring(game:HttpGet("https://raw.githubusercontent.com/CoriMyp/ParadoxHub/main/Standart/standart.lua"))()
+	MoveSec:Button("Change walk speed", function()
+		source:walk_speed()
+	end)
+
+	MoveSec:Slider("Jump power", 50, 300, 0, 1, "JumpPower Slider", function(value)
+		source.jumppower_value = value
+	end)
+
+	MoveSec:Button("Change JumpPower", function()
+		source:jump_power()
+	end)
+
+	MoveSec:Toggle("Infinity jump", false, "InfJump Toggle", function(bool)
+		
+	end)
+
+	
+	local FlySec = tab:Section("Fly")
+
+	FlySec:Toggle("Fly", false, "Fly Toggle", function(bool) 
+		source:fly(bool)
+	end)
+
+	FlySec:Bind("Fly button", Enum.KeyCode.F, false, "Fly Bind", function (keycode)
+		source.fly_keycode = keycode
+	end)
+
+	FlySec:Slider("Fly speed", 50, 500, 50, 1, "FlySpeed Slider", function(value)
+		source.fly_speed = value
+	end)
+
 end
-
-local win = lib:New({
-	Name = "Paradox Hub",
-	FolderToSave = "ParadoxHub"
-})
-
-local PlayerTab = win:Tab("Player")
-
-
-code:run(PlayerTab)
-
