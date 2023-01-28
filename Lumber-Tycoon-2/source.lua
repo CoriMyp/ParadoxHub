20d01812e83dc3507d4effda4220ffe52ebeb2ef
CoriMyp <81583396+CoriMyp@users.noreply.github.com>
2023-01-28 19:52:42 +0200

    Create source.lua

---
+++ Lumber-Tycoon-2/source.lua
@@ -0,0 +1,81 @@
+wood_types = {
+	["Elm"] = "Generic",
+	["Oak"] = "Oak",
+	["Cherry"] = "Cherry",
+	["Walnut"] = "Walnut",
+	["Birch"] = "Birch",
+	["Fir"] = "Fir",
+	["Pine"] = "Pine",
+	["Koa"] = "Koa",
+	["Lava"] = "Volcano",
+	["Zombie"] = "GreenSwampy",
+	["Gold"] = "GoldSwampy",
+	["Frost"] = "Frost",
+	["Cavecrawler"] = "CaveCrawler",
+	["Palm"] = "Palm",
+	["Snowglow"] = "SnowGlow",
+	["Phantom"] = "LoneCave"
+}
+
+woods = {
+	"Elm", "Oak", "Cherry", "Walnut", "Birch", "Fir", "Pine", "Koa", "Lava", "Zombie", "Gold", "Frost", "Cavecrawler", "Palm", "Snowglow", "Phantom"
+}
+
+
+axes_damage = {
+	["ManyAxe"] = {10.2},
+	["EndTimesAxe"] = {1.58, 10000000},
+	["AxeSwamp"] = {0.8, 7, 5.35},
+	["FireAxe"] = {0.4, 8.12},
+	["GingerbreadAxe"] = {1.2, 11, 8.5},
+	["CaveAxe"] = {0.4, 7.2},
+	["IceAxe"] = {0.36, 6},
+	["AxeTwitter"] = {1.55, 2.5, 3.9},
+
+	["RustyAxe"] = {0.55},
+	["CandyCornAxe"] = {1.75},
+	["AxeBetaTesters"] = {1.5},
+	["CandyCaneAxe"] = {0},
+	["AxeAmber"] = {3.73},
+	["Beesaxe"] = {0.95},
+	["AxeAlphaTesters"] = {1.45},
+	["AxeChicken"] = {0.75},
+	["Rukiryaxe"] = {1.68},
+
+	["BasicHatchet"] = {0.2},
+	["Axe1"] = {0.55},
+	["Axe2"] = {0.93},
+	["Axe3"] = {1.45},
+	["SilverAxe"] = {1.6}
+}
+
+local axes = {
+	"ManyAxe",
+	"EndTimesAxe",
+	"AxeSwamp",
+	"FireAxe",
+	"GingerbreadAxe",
+	"CaveAxe",
+	"IceAxe",
+	"AxeTwitter",
+
+	"RustyAxe",
+	"CandyCornAxe",
+	"AxeBetaTesters",
+	"CandyCaneAxe",
+	"AxeAmber",
+	"Beesaxe",
+	"AxeAlphaTesters",
+	"AxeChicken",
+	"Rukiryaxe",
+
+	"BasicHatchet",
+	"Axe1",
+	"Axe2",
+	"Axe3",
+	"SilverAxe"
+}
+
+waypoints = {
+
+}
